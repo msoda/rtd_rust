90d423f5c52d628caa14afad43415ebb3eac272e
msoda <alexquino@pm.me>
2021-05-13 20:53:41 -0500

    first commit

---
+++ src/main.rs
@@ -0,0 +1,37 @@
+use std::io;
+use rand::Rng;
+
+fn main() {
+    loop {
+        println!("--Input--");
+        println!("Enter amount of dice to roll:");
+
+        let mut d_amt: String = String::new();
+        io::stdin().read_line(&mut d_amt)
+            .expect("Failed to read line");
+
+        let d_amt: u32 = match d_amt.trim().parse() {
+            Ok(amt) => amt,
+            Err(_) => {
+                println!("Must be a number.");
+                continue;
+            },
+        };
+
+        let mut vec:Vec<u32> = Vec::new();
+
+        for _i in 0..d_amt {
+            let r_num: u32 = rand::thread_rng()
+                .gen_range(1, 7);
+
+            vec.push(r_num);
+        }
+
+        println!("\n--Output--");
+
+        for r in 0..vec.len() {
+            println!("Roll #{}: {}", r+1, vec[r]);
+        }
+        break;
+    }
+}
